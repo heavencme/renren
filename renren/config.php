b345cc7636b0e575051da802ff003ec83d2764a1
heavencme <heavencme@gmail.com>
2013-08-13 22:51:06 +0800

    renren api 2.0

---
+++ renren/config.php
@@ -0,0 +1,9 @@
+<?php
+header ( 'Content-Type: text/html; charset=UTF-8' );
+// App Key
+define ( "APP_KEY", 'YOUR APP KEY' );
+// App Secret
+define ( "APP_SECRET", 'YOUR APP SECRET' );
+// 应用回调页地址
+define ( "CALLBACK_URL", "YOUR CALLBACK URL" );
+
